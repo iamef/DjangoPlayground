531f48a8636810c4a344883c2d7340ffcae468a4
Emily Fan <emilyfan8@gmail.com>
2020-01-22 00:24:57 -0500

    Add comment in blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     """
     :param models.Model illustrates that it is a Django model
     """
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # Foreign key means that it links to another model
     title = models.CharField(max_length=200)  # text with a limited amount of characters
     text = models.TextField()  # text without any limit on number of characters
     created_date = models.DateTimeField(default=timezone.now)
