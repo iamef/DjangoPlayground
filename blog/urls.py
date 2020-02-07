07f80a1a5fa3fb8bd1b4ee2e7660f27bcf45bf62
Emily Fan <emilyfan8@gmail.com>
2020-02-07 17:14:29 -0500

    Add comment in blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.post_list, name='post_list'),
+    path("", views.post_list, name='post_list'),  # tells Django that views.posts_list is the right place to go
+    # views is a python file
+    # post_list is a function in views
 ]
