dce855907c96442f8a2947c487daa558cecda34c
Emily Fan <emilyfan8@gmail.com>
2020-02-07 18:51:52 -0500

    Give individual posts individual pages

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.post_list, name='post_list'),  # tells Django that views.posts_list is the right place to go
     # views is a python file
-    # post_list is a function in views
+    path("", views.post_list, name='post_list'),  # tells Django that views.posts_list is the right place to go
+    # post_list is a function in views.py
+
+    path('post/<int:pk>/', views.post_detail, name="post_detail")
+    # post_detail is a function is views.py
 ]
