1a5b9606e7473f5b302e0d29c7dbafd0fbece747
Emily Fan <emilyfan8@gmail.com>
2020-03-21 16:47:30 -0700

    Add form page to make new post
    forms.py
    create a file for views.py to use for making new post
    
    views.py
    Add a post_new method for making new post
    
    urls.py
    Add a url for a new post page
    
    base.html
    Add link to page to make a new post

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path("", views.post_list, name='post_list'),  # tells Django that views.posts_list is the right place to go
     # post_list is a function in views.py
 
-    path('post/<int:pk>/', views.post_detail, name="post_detail")
+    path('post/<int:pk>/', views.post_detail, name="post_detail"),
     # post_detail is a function is views.py
+
+    path('post/new/', views.post_new, name='post_new')
+
 ]
