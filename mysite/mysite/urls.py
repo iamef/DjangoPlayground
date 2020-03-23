a8d88e8e5bbf160e4284449737d7b1fe750a0a8b
Emily Fan <emilyfan8@gmail.com>
2020-03-23 01:05:08 -0700

    Add polls to URL
    mysite/urls.py
    Add polls/ path that sends stuff to polls.url
    
    polls/urls.py
    ask django to run index from views.py
    
    polls/views.py (whoops edits in last commit)
    add index() method for /polls page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls'))  # include() takes a full Python import path to another URLconf module
+    # include() chops off whatever part of the URL matched up to that point
+    # and sends the remaining string to the included URLconf for further processing
 ]
