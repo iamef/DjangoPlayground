d16c35ae7af85407b493425233fb3cc6a3a9b321
Emily Fan <emilyfan8@gmail.com>
2020-03-23 21:37:55 -0700

    Add custom function in Question model
    models.py
    def was_published_recently

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import datetime
+
 from django.db import models
 
 # Create your models here.
 # each model has class variables
 # each variable represents a database field
+from django.utils import timezone
 
 
 class Question(models.Model):
     # question_text and pub_date are field names
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    def was_published_recently(self):
+        return timezone.now() - self.pub_date <= datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question_text
