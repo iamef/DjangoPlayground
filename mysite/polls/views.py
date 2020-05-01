fbb486fcfaf6800427780dbc853f6e027cde071d
Emily Fan <emilyfan8@gmail.com>
2020-04-30 20:37:03 -0700

    Have views use Question model in index 8000/polls/ will now display the last 5 question

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,9 +4,20 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
+# all Django wants is an HttpResponse or a Http404
+# this is different from what DjangoGirls did with render
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # get the 5 latest updates
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    output = ', '.join([q.question_text for q in latest_question_list])
+
+    return HttpResponse(output)
+    # return HttpResponse("Hello, world. You're at the polls index.")
 
 
 def details(request, question_id):
